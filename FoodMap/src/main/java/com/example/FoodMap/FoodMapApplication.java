bc42cab74a54aeadca2cfb4c37e657af3ba343be
jlucas86 <joshua.lucas014@gmail.com>
2022-05-28 04:03:41 -0400

    no change

---
+++ FoodMap/src/main/java/com/example/FoodMap/FoodMapApplication.java
@@ -0,0 +1,17 @@
+package com.example.FoodMap;
+
+import com.example.FoodMap.componets.users.users;
+import com.example.FoodMap.componets.users.usersService;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FoodMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMapApplication.class, args);
+	}
+}
